af9f657328ed6bdd527a735bf2fbb1ad736d2684
Landen Bolich <landendbolich@gmail.com>
2024-12-01 00:57:09 -0500

    feat: 2024 d1p2

---
+++ 2024/Day 1/d1p2.py
@@ -0,0 +1,19 @@
+def main():
+    with open('input.txt') as file:
+        lines = file.readlines()
+
+    lowest_left_numbers = sorted(int(line.split(' ')[0]) for line in lines)
+    lowest_right_numbers = sorted(int(line.split(' ')[3]) for line in lines)
+    similarity_score = 0
+
+    for i in range(len(lowest_left_numbers)):
+        num_times_appeared = 0
+        for j in range(len(lowest_right_numbers)):
+            if lowest_left_numbers[i] == lowest_right_numbers[j]:
+                num_times_appeared += 1
+        similarity_score += (lowest_left_numbers[i] * num_times_appeared)
+
+    print(similarity_score)
+
+if __name__ == '__main__':
+    main()
